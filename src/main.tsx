5e98e83c75661b22e28e50a403bddcd9d4244563
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-19 20:58:54 +0200

    Added initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.tsx';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const router = createHashRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <div>ErrorPage</div>,
+        children: [
+            {
+                index: true,
+                element: <div>HomePage</div>,
+            },
+            {
+                path: '/сategories',
+                element: <div>CategoriesPage</div>,
+
+                children: [
+                    {
+                        path: '/:category',
+                        element: <div>CatalogPage</div>,
+                    },
+                ],
+            },
+            {
+                path: '/contacts',
+                element: <div>ContactsPage</div>,
+            },
+            {
+                path: '/product/:id',
+                element: <div>ProductPage</div>,
+            },
+        ],
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
