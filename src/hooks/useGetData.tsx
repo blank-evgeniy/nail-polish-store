5fd169694a66f8450cfce7a713f252b4367210b1
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-21 09:38:13 +0200

    Firebase is integrated to work with the database, added custom hooks for getting data

---
+++ src/hooks/useGetData.tsx
@@ -0,0 +1,69 @@
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    Query,
+    query,
+} from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import { useEffect, useState } from 'react';
+
+export const useGetDoc = <T,>(path: string, id: string) => {
+    const [data, setData] = useState<T | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const docRef = doc(db, path, id);
+        getDoc(docRef)
+            .then((response) => {
+                if (!response.exists()) {
+                    throw Error('Данные не найдены');
+                }
+                setIsLoading(false);
+                setData(response.data() as T);
+                setError(null);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                setError(error.message);
+            });
+    }, [path, id]);
+
+    return { data, isLoading, error };
+};
+
+export const useGetDocs = <T,>(path: string) => {
+    const [data, setData] = useState<T[] | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const q = query(collection(db, path)) as Query<T>;
+        getDocs(q)
+            .then((response) => {
+                if (response.empty) {
+                    throw Error('Данные не найдены');
+                }
+                const newData: T[] = [];
+
+                response.forEach((doc) => {
+                    newData.push({
+                        ...doc.data(),
+                        id: doc.id,
+                    });
+                });
+
+                setIsLoading(false);
+                setData(newData);
+                setError(null);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                setError(error.message);
+            });
+    }, [path]);
+
+    return { data, isLoading, error };
+};
--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,47 +1,60 @@
 import { Link } from 'react-router-dom';
+import { useGetDocs } from '../../hooks/useGetData';
 
-const categories = [
-    {
-        title: 'Базы',
-        link: 'basy',
-    },
-    {
-        title: 'Топы',
-        link: 'topy',
-    },
-];
+interface CategoriesData {
+    title: string;
+    image: string;
+    id: string;
+}
 
 const Categories = () => {
+    const { data, isLoading, error } = useGetDocs<CategoriesData>('sections');
+
     return (
         <div className="container-lg">
             <h1 className="text-center my-5">Категории товаров</h1>
-            <div className="row">
-                {categories.map((category) => (
-                    <div className="col-md-3">
-                        <div
-                            className="card mb-4 flex-d flex-column align-items-center"
-                            style={{ width: '240px' }}
-                        >
-                            <img
-                                src="basy.jpg"
-                                className="mt-3"
-                                style={{ width: '200px', height: '200px' }}
-                            ></img>
-                            <div className="card-body">
-                                <h5 className="card-title text-center">
-                                    {category.title}
-                                </h5>
-                                <Link
-                                    to={`/categories/${category.link}`}
-                                    className="btn btn-primary"
-                                >
-                                    Перейти к категории
-                                </Link>
+            {isLoading ? (
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            ) : error ? (
+                <p>{error}</p>
+            ) : !data ? (
+                <p>Товары не найдены</p>
+            ) : (
+                <div className="row">
+                    {data.map((category) => (
+                        <div key={category.id} className="col-md-3">
+                            <div
+                                className="card mb-4 flex-d flex-column align-items-center"
+                                style={{ width: '240px' }}
+                            >
+                                <img
+                                    src="basy.jpg"
+                                    className="mt-3"
+                                    style={{
+                                        width: '200px',
+                                        height: '200px',
+                                    }}
+                                ></img>
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">
+                                        {category.title}
+                                    </h5>
+                                    <Link
+                                        to={`/categories/${category.id}`}
+                                        className="btn btn-primary"
+                                    >
+                                        Перейти к категории
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
