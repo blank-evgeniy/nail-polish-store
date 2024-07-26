208a6678371f26f6ca290abd32c4a87ab743c390
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-26 22:36:13 +0200

    Breadcrumb has been added to the product catalog page, the catalog page has been decomposed

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { ProductCardsSkeleton } from './Skeleton';
+import filterProducts from '../auxiliary/filterProducts';
+import searchProducts from '../auxiliary/searchProducts';
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import ProductCard from './ProductCard';
+import { useLocation } from 'react-router-dom';
+import { filterSlice } from '../store/reducers/filterSlice';
+import ProductData from '../types/ProductData';
+
+interface ProductsListProps {
+    productsData: ProductData[] | undefined;
+    isLoading: boolean;
+    isError: boolean;
+}
+
+const ProductsList: React.FC<ProductsListProps> = ({
+    productsData,
+    isError,
+    isLoading,
+}) => {
+    const location = useLocation();
+
+    const { searchValue, volumeFilter, colorFilter } = useAppSelector(
+        (state) => state.filter
+    );
+    const { volumeFilterUpdate, colorFilterUpdate, searchValueUpdate } =
+        filterSlice.actions;
+    const dispatch = useAppDispatch();
+
+    const filteredProducts = filterProducts(
+        searchProducts(searchValue, productsData),
+        volumeFilter,
+        colorFilter
+    );
+
+    useEffect(() => {
+        dispatch(volumeFilterUpdate('DEFAULT'));
+        dispatch(colorFilterUpdate('DEFAULT'));
+        dispatch(searchValueUpdate(''));
+    }, [location]);
+
+    return (
+        <div className="row">
+            {isLoading && <ProductCardsSkeleton />}
+            {isError && (
+                <div className="text-center">
+                    Произошла непредвиденная ошибка
+                </div>
+            )}
+            {productsData &&
+                filteredProducts.map((product) => (
+                    <div
+                        key={product.id}
+                        className="col-xxl-2 col-xl-3 col-sm-4 col-6"
+                    >
+                        <ProductCard {...product} key={product.id} />
+                    </div>
+                ))}
+        </div>
+    );
+};
+
+export default ProductsList;
--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,35 +1,13 @@
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getProducts } from '../api/getProducts';
-import ProductCard from '../components/ProductCard';
 import { getDocQuery } from '../api/getData';
 import CategoriesData from '../types/CategoriesData';
-import { ProductCardsSkeleton } from '../components/Skeleton';
 import FilterForm from '../components/FilterForm';
-import { useAppDispatch, useAppSelector } from '../hooks/redux';
-import searchProducts from '../auxiliary/searchProducts';
-import filterProducts from '../auxiliary/filterProducts';
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import { filterSlice } from '../store/reducers/filterSlice';
+import ProductsList from '../components/ProductsList';
 
 const CatalogPage = () => {
-    const { searchValue, volumeFilter, colorFilter } = useAppSelector(
-        (state) => state.filter
-    );
-
     const { category } = useParams<string>();
-    const location = useLocation();
-
-    const { volumeFilterUpdate, colorFilterUpdate, searchValueUpdate } =
-        filterSlice.actions;
-    const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        dispatch(volumeFilterUpdate('DEFAULT'));
-        dispatch(colorFilterUpdate('DEFAULT'));
-        dispatch(searchValueUpdate(''));
-    }, [location]);
 
     const { data: categoryData } = useQuery(category!, () =>
         getDocQuery<CategoriesData>('categories-demo', category!)
@@ -41,15 +19,21 @@ const CatalogPage = () => {
         isError,
     } = useQuery(['products', category], () => getProducts(category!));
 
-    const filteredProducts = filterProducts(
-        searchProducts(searchValue, productsData),
-        volumeFilter,
-        colorFilter
-    );
-
     return (
-        <div className="container-xl">
-            <h1 className="text-center my-5">
+        <div className="container-xl p-3">
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb fs-5">
+                    <li className="breadcrumb-item">
+                        <Link to="/categories" className="link-secondary">
+                            Все категории
+                        </Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                        {categoryData?.title}
+                    </li>
+                </ol>
+            </nav>
+            <h1 className="text-center mt-2 mb-5">
                 {categoryData ? (
                     categoryData.title
                 ) : (
@@ -59,23 +43,11 @@ const CatalogPage = () => {
 
             <FilterForm data={productsData} />
 
-            <div className="row">
-                {isLoading && <ProductCardsSkeleton />}
-                {isError && (
-                    <div className="text-center">
-                        Произошла непредвиденная ошибка
-                    </div>
-                )}
-                {productsData &&
-                    filteredProducts.map((product) => (
-                        <div
-                            key={product.id}
-                            className="col-xxl-2 col-xl-3 col-sm-4 col-6"
-                        >
-                            <ProductCard {...product} key={product.id} />
-                        </div>
-                    ))}
-            </div>
+            <ProductsList
+                productsData={productsData}
+                isError={isError}
+                isLoading={isLoading}
+            />
         </div>
     );
 };
