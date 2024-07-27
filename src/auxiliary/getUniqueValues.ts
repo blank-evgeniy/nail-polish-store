f037f4ce815b807dfdb56a0d65937b2d05c53880
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-27 13:28:50 +0200

    Images for new products have been uploaded, the function for obtaining unique values has been improved

--- src/auxiliary/getUniqueValues.ts
+++ src/auxiliary/getUniqueValues.ts
@@ -2,9 +2,9 @@ const getUniqueValues = (
     listValues: (string | number | undefined)[] | undefined | null
 ) => {
     if (!listValues) return [];
-    return listValues.filter(
-        (item, index, array) => array.indexOf(item) === index
-    );
+    return listValues
+        .filter((item, index, array) => array.indexOf(item) === index)
+        .sort();
 };
 
 export default getUniqueValues;
