1be789d1de656bf959ee006f4683fb942846314f
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-08-06 19:37:29 +0200

    Added an exit button from the admin panel

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -2,7 +2,16 @@ const AdminHeader = () => {
     return (
         <header>
             <div className="container-fluid bg-secondary py-3">
-                <h1 className="text-primary fs-2">Административная панель</h1>
+                <div className="row">
+                    <h1 className="text-primary fs-2 col-11">
+                        Административная панель
+                    </h1>
+                    <div className="col-1">
+                        <a className="btn btn-primary" href="/">
+                            Выйти
+                        </a>
+                    </div>
+                </div>
             </div>
         </header>
     );
